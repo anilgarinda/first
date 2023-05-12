eca79b36fe3f2a4e011f898d68e4b161f26cb233
anilgarinda <114723085+anilgarinda@users.noreply.github.com>
2023-05-13 00:59:10 +0530

    Update app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title('My Web App')
+st.header('Header')
+st.subheader('Subheader')
+st.write('Hello, World!')
+if st.button('Click me'):
+    st.write('Button clicked!')
+if st.checkbox('Checkbox'):
+    st.write('Checkbox checked!')
+option = st.selectbox('Select an option', ['Option 1', 'Option 2', 'Option 3'])
+st.write('You selected:', option)
+value = st.slider('Slide me', 0, 10)
+st.write('You selected:', value)
+file = st.file_uploader('Upload file', type=['csv', 'txt'])
+if file:
+    st.write(file.read())
