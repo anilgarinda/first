163c30165cd30e33cbb8694fc734e1bea7b0cf69
anilgarinda <114723085+anilgarinda@users.noreply.github.com>
2023-06-16 11:35:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
+import os
 import streamlit as st
 from pytube import YouTube
 
-
-def download_video(video_url):
+def download_video(video_url, output_path):
     try:
         yt = YouTube(video_url)
         video = yt.streams.get_highest_resolution()
-        video_path = video.download()
-        message = f"Video downloaded successfully! Saved at: {video_path}"
+        video.download(output_path=output_path)
+        return True, f"Video downloaded successfully! Saved at: {output_path}"
     except Exception as e:
-        message = f"Error occurred: {str(e)}"
-
-    return message
-
+        return False, f"Error occurred: {str(e)}"
 
 def main():
-    st.title("YouTube Video Downloader created by bhoi")
-    # Input field
-    video_url = st.text_input("jo  YouTube video download krna h uski link daal:")
+    st.title("YouTube Video Downloader")
+    video_url = st.text_input("Enter the YouTube video URL:")
+    output_path = st.text_input("Enter the output path:")
+    download_button = st.button("Download Video")
 
-    # Download button
-    if st.button("Download"):
-        if video_url:
-            message = download_video(video_url)
-            st.success(message)
+    if download_button:
+        if video_url and output_path:
+            success, message = download_video(video_url, output_path)
+            st.write(message)
         else:
-            st.error("Please provide the video URL.")
-
+            st.write("Please provide both the video URL and output path.")
 
 if __name__ == "__main__":
     main()
