90ab6b05f8b35566d88a7ccbbf285e0e45c59e75
anilgarinda <114723085+anilgarinda@users.noreply.github.com>
2023-06-16 11:06:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,33 @@
 import streamlit as st
-st.title('My Web App')
-st.header('Header')
-st.subheader('Subheader')
-st.write('Hello, World!')
-if st.button('Click me'):
-    st.write('Button clicked!')
-if st.checkbox('Checkbox'):
-    st.write('Checkbox checked!')
-option = st.selectbox('Select an option', ['Option 1', 'Option 2', 'Option 3'])
-st.write('You selected:', option)
-value = st.slider('Slide me', 0, 10)
-st.write('You selected:', value)
-file = st.file_uploader('Upload file', type=['csv', 'txt'])
-if file:
-    st.write(file.read())
-import streamlit as st
 from pytube import YouTube
 
 
-# Set page title
-st.title("YouTube Video Downloader")
-
-# Get YouTube video URL from user input
-video_url = st.text_input("Enter YouTube Video URL")
-
-if st.button("Download"):
+def download_video(video_url):
     try:
-        # Create a YouTube object
         yt = YouTube(video_url)
-
-        # Get the highest resolution video
         video = yt.streams.get_highest_resolution()
-
-        # Set the output path for the downloaded video
-        output_path = "./video.mp4"  # You can change the output path here
-
-        # Download the video
-        video.download(output_path)
-
-        st.success("Video downloaded successfully!")
-        st.markdown(f"Downloaded video: [video.mp4](./video.mp4)")
+        video_path = video.download()
+        message = f"Video downloaded successfully! Saved at: {video_path}"
     except Exception as e:
-        st.error(f"An error occurred: {str(e)}")
+        message = f"Error occurred: {str(e)}"
+
+    return message
 
 
+def main():
+    st.title("YouTube Video Downloader")
+
+    # Input field
+    video_url = st.text_input("Enter the YouTube video URL:")
+
+    # Download button
+    if st.button("Download"):
+        if video_url:
+            message = download_video(video_url)
+            st.success(message)
+        else:
+            st.error("Please provide the video URL.")
+
+
+if __name__ == "__main__":
+    main()
