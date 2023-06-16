b8e412572a0a2f0ec2c74078a5734e3130e3ce6f
anilgarinda <114723085+anilgarinda@users.noreply.github.com>
2023-06-16 11:38:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 from pytube import YouTube
+from pathlib import Path
 
 def download_video(video_url, output_path):
     try:
@@ -14,15 +15,20 @@ def download_video(video_url, output_path):
 def main():
     st.title("YouTube Video Downloader")
     video_url = st.text_input("Enter the YouTube video URL:")
-    output_path = st.text_input("Enter the output path:")
+    output_path = None
+
+    if st.button("Choose Output Path"):
+        output_path = st.file_uploader("Select output path", type="directory", key="output_path")
+
     download_button = st.button("Download Video")
 
     if download_button:
         if video_url and output_path:
-            success, message = download_video(video_url, output_path)
+            output_path = Path(output_path.name).resolve()
+            success, message = download_video(video_url, str(output_path))
             st.write(message)
         else:
-            st.write("Please provide both the video URL and output path.")
+            st.write("Please provide both the video URL and select the output path.")
 
 if __name__ == "__main__":
     main()
